a133e9e1e54dbb61902c2377dcf4b405756a3f8d
Ethan <marchand.ethan@bluewin.ch>
2024-10-04 14:38:11 +0200

    Correction
    Ajout du check user au moment de recommencer O/N

--- main.cpp
+++ main.cpp
@@ -15,44 +15,44 @@ int main() {
     int nbr_max;
     int nombre;
     int compteur;
-    bool exit = false;
     char result;
 
     do {
-        do {
-            cout << "Enter une valeur [2-1000]" << endl;
-            cin >> nbr_max;
+        cout << "Enter une valeur [2-1000]" << endl;
+        cin >> nbr_max;
 
-        } while (nbr_max < 2 || nbr_max > 1000);
+    } while (nbr_max < 2 || nbr_max > 1000);
 
-        cout << "Voici la liste des nombres premiers" << endl;
-        while(nombre < nbr_max){
-            compteur = 0;
-            nombre++;
+    cout << "Voici la liste des nombres premiers" << endl;
+    nombre = 0;
+    while(nombre < nbr_max){
+        compteur = 0;
+        nombre++;
 
-            for (int i = 1 ; i <= nombre ; i++)
-            {
-                if ((nombre % i) == 0)
-                    compteur++;
-            }
-
-            if(compteur == 2)  //Le nombre premier se divise sur 1 et sur lui meme
-            {
-                cout << nombre << endl;
-            }
+        for (int i = 1 ; i <= nombre ; i++)
+        {
+            if ((nombre % i) == 0)
+                compteur++;
         }
 
+        if(compteur == 2)  //Le nombre premier se divise sur 1 et sur lui meme
+        {
+            //cout << nombre << endl;
+            cout << '\t' << nombre << " " << '\t' << endl;
+        }
+    }
+
+    do {
         cout << "Voulez-vous recommencer [O/N]" << endl;
         cin >> result;
-        if(result == 'O' || result == 'o')
-        {
-            exit = true;
-        }
-        else
-        {
-            exit = false;
-        }
-    } while(exit);
+    } while(result != 'O' && result != 'o' && result != 'N' && result != 'n');
 
-    return EXIT_SUCCESS;
+    if(result == 'O' || result == 'o')
+    {
+        return main();
+    }
+    else
+    {
+        return EXIT_SUCCESS;;
+    }
 }
